9751a5a4a829d90a3fe6839f7a79d693e1da090a
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-05-09 11:05:28 -0400

    Initial commit

---
+++ Tests/FakeStoreCoreTests/FakeStoreCoreTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import FakeStoreCore
+
+final class FakeStoreCoreTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
