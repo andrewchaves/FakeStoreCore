b63ae5dc2b124b0b9d4e16e11f3c39fab0db2583
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-05-13 23:27:07 -0400

    remove useless comment

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FakeStoreCore",
     platforms: [
-            .iOS(.v13) // 👈 Adicione esta linha
+            .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
