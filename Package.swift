85ed13e9130bfb8dd9692d1174b9463ff09c1ea9
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-05-13 23:25:26 -0400

    Support only iOS13+

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "FakeStoreCore",
+    platforms: [
+            .iOS(.v13) // 👈 Adicione esta linha
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
